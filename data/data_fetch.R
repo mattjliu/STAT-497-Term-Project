42fa2cf5196abff0777d7b05e3b877ca097e7e81
matthewliu96 <matthew.liu041296@gmail.com>
2019-03-14 14:13:47 -0400

    update fetch script

--- data/data_fetch.R
+++ data/data_fetch.R
@@ -32,4 +32,38 @@ get_data <- function(curr,start,end,period,length){
     return(series)
 }
 
-series <- get_data("ETH",params$start,params$end,params$period,52)
+ETH <- get_data("ETH",params$start,params$end,params$period,52)
+XRP <- get_data("XRP",params$start,params$end,params$period,52)
+XMR <- get_data("XMR",params$start,params$end,params$period,52)
+LTC <- get_data("LTC",params$start,params$end,params$period,52)
+
+high <- cbind(ETH$high,XRP$high,XMR$high,LTC$high)
+low <- cbind(ETH$low,XRP$low,XMR$low,LTC$low)
+open <- cbind(ETH$open,XRP$open,XMR$open,LTC$open)
+close <- cbind(ETH$close,XRP$close,XMR$close,LTC$close)
+
+# Rename all the columns
+colnames(high)  <- currency_vec
+colnames(low) <- currency_vec
+colnames(open) <- currency_vec
+colnames(close) <- currency_vec
+prices_vec <- list(high,low,open,close)
+
+# function that returns price relative vector (yt)
+# params: previous vector and current vector
+getPriceRelativeVec <- function(prev_v,curr_v)
+    return(c(1,curr_v)/c(1,prev_v))
+
+# function that returns normalized price matrix
+# params: prices = dataframe (hi,lo,close), vt = closing prices, n = number of timesteps in the past
+# prices dataframe must end at time t
+getPriceMatrix <- function(prices,vt,n){
+    mat <- matrix(tail(prices,n),ncol=4)
+    vec <- as.vector(vt)
+    return(t(sweep(mat,2,vec,'/')))
+}
+
+# function that returns log returns given price-relative vector and weights vector
+# params: yt = price-relative vector at time t, wt = weight vector at time t
+getLogReturns <- function(yt,wt)
+    return(log(dot(yt,wt)))
